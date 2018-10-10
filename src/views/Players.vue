ce00425ed1058cad8d50b045940f8d7ff1ae087d
James Quigley <squigs13@gmail.com>
2018-10-10 14:21:32 +0100

    Adding axios for data retrieval

--- src/views/Players.vue
+++ src/views/Players.vue
@@ -1,53 +1,79 @@
 <template>
     <b-container>
-        <b-card no-body>
-            <b-tabs card>
-                <b-tab title="GK" active>
-                    <b-table responsive striped :items="players.gk" :fields="fields" sort-direction="desc" sort-by="Club">
-                        <template slot="Name" slot-scope="data">
-                            <router-link :to="`/players/${data.item.id}`">
-                                {{data.value}}
-                            </router-link>
-                        </template>
-                    </b-table>
-                </b-tab>
-                <b-tab title="DF">
-                    <b-table responsive striped :items="players.df" :fields="fields" sort-direction="desc" sort-by="Club">
-                        <template slot="Name" slot-scope="data">
-                            <router-link :to="`/players/${data.item.id}`">
-                                {{data.value}}
-                            </router-link>
-                        </template>
-                    </b-table>
-                </b-tab>
-            </b-tabs>
+        <b-row>
+            <b-col md="6">
+                <b-form-group horizontal label="Filter">
+                <b-input-group>
+                    <b-form-input v-model="filter" placeholder="Type to Search" />
+                    <b-input-group-append>
+                    <b-btn :disabled="!filter" @click="filter = ''">Clear</b-btn>
+                    </b-input-group-append>
+                </b-input-group>
+                </b-form-group>
+            </b-col>
+            <b-col md="6">
+                <b-pagination :total-rows="totalRows" :per-page="perPage" v-model="currentPage" />
+            </b-col>
+        </b-row>
+        <b-card>
+            <b-table 
+                responsive
+                striped
+                bordered
+                :items="players"
+                :fields="fields"
+                sort-direction="desc"
+                sort-by="teamID"
+                :per-page="perPage"
+                :current-page="currentPage"
+                :filter="filter"
+                @filtered="onFiltered">
+                <template slot="Name" slot-scope="data">
+                    <router-link :to="`/players/${data.item.ID}`">
+                        <span v-html="data.item.firstname + ' ' + data.item.lastname"></span>
+                    </router-link>
+                </template> 
+            </b-table>
         </b-card>
     </b-container>
 </template>
 <script>
+import axios from 'axios'
+
 export default {
   data () {
     return {
-      players: {
-        gk: [
-          { id: 254839, Name: 'Petr Cech', Club: 'ARS', Mins: 90, Tackles: 6, Passes: 24, Goals: 0, Assists: 0, Pts: 0 },
-          { id: 34785, Name: 'David de Gea', Club: 'MNU', Mins: 90, Tackles: 4, Passes: 43, Goals: 0, Assists: 0, Pts: 3 }
-        ],
-        df: [
-          { id: 76284, Name: 'Harry Maguire', Club: 'LEI', Mins: 90, Tackles: 6, Passes: 43, Goals: 0, Assists: 0, Pts: 1 },
-          { id: 7384, Name: 'Chris Smalling', Club: 'MNU', Mins: 90, Tackles: 4, Passes: 34, Goals: 0, Assists: 1, Pts: 3 }
-        ]
-      },
+      players: [],
       fields: [
-        { key: 'Name', sortable: true, sortDirection: 'asc' },
-        { key: 'Club', sortable: true, sortDirection: 'asc' },
-        { key: 'Mins', sortable: true },
-        { key: 'Tackles', sortable: true },
-        { key: 'Passes', sortable: true },
-        { key: 'Goals', sortable: true },
-        { key: 'Assists', sortable: true },
-        { key: 'Pts', sortable: true }
-      ]
+        { key: 'Name' },
+        { key: 'teamID', label:"Club", sortable: true, sortDirection: 'asc' },
+        { key: 'mins', sortable: true },
+        { key: 'tackles', sortable: true },
+        { key: 'passes', sortable: true },
+        { key: 'goals', sortable: true },
+        { key: 'assists', sortable: true },
+        { key: 'cleansheets', label: "CS", sortable: true },
+        { key: 'pts', sortable: true }
+      ],
+      currentPage: 1,
+      perPage: 50,
+      filter: null,
+      totalRows: 50
+    }
+  },
+  created() {
+    axios
+      .get('https://sffl-data.firebaseio.com/players.json')
+      .then(response => {
+          this.players = response.data.players
+          this.totalRows = this.players.length
+      })
+  },
+  methods: {
+      onFiltered (filteredItems) {
+      // Trigger pagination to update the number of buttons/pages due to filtering
+      this.totalRows = filteredItems.length
+      this.currentPage = 1
     }
   }
 }
--- src/views/Standings.vue
+++ src/views/Standings.vue
@@ -2,27 +2,25 @@
     <b-container>
         <b-card bg-variant="light"
                 header="Current Standings">
-            <b-table striped small :items="items" :fields="fields" sort-by="Pts" sort-desc="true"></b-table>
+            <b-table striped small :items="items" :fields="fields" sort-by="Pts" sort-desc></b-table>
         </b-card>
     </b-container>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   data () {
     return {
-      items: [
-        { Team: 'Rovers', Pts: 20, Diff: '+3' },
-        { Team: 'United', Pts: 18, Diff: '-0.5' },
-        { Team: 'City', Pts: 14, Diff: '0' },
-        { Team: 'Town', Pts: 10, Diff: '+0.5' },
-        { Team: 'Athletic', Pts: 8, Diff: '+1.5' },
-        { Team: 'County', Pts: 27, Diff: '-2.5' },
-        { Team: 'Wanderers', Pts: 7, Diff: '-2' },
-        { Team: 'Forest', Pts: 5, Diff: '+1' }
-      ],
+      items: [],
       fields: ['Team', 'Pts', { key: 'Diff', label: '+/-' }]
     }
+  },
+  created() {
+    axios
+      .get('https://sffl-data.firebaseio.com/standings.json')
+      .then(response => (this.items = response.data))
   }
 }
 </script>
